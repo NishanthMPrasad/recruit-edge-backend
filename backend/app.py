f259971f9eb36f828852a4cc44d5bd5412f1a54c
nismahen <nismahen@iu.edu>
2025-08-13 21:07:16 -0400

    Update app.py to use dynamic PORT for Render

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+'''
 # backend/app.py
 from flask import Flask
 from flask_cors import CORS
@@ -13,3 +14,23 @@ app.register_blueprint(api_bp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+'''
+
+# backend/app.py
+from flask import Flask
+from flask_cors import CORS
+from .routes import api_bp  # Import the blueprint
+import os
+
+app = Flask(__name__)
+
+# This allows your React app to make requests to your Flask app
+CORS(app)
+
+# Register the blueprint
+app.register_blueprint(api_bp, url_prefix='/api')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
